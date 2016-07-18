53c2b18502441b6ce82f6416a7187f2d0c6d19ce
annpod <ann@podgmail.com>
2016-07-18 23:46:15 +0300

    added order

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,7 @@
 var app = angular.module("todoApp", []);
 app.controller("todoCtrl", function($scope, $http){
+	$scope.fieldName = undefined;
+	$scope.reverse = false;
 
 	/*init data*/
 	$http.get('tasks.json').success(function(data, status, headers, config){
@@ -10,12 +12,29 @@ app.controller("todoCtrl", function($scope, $http){
 		});
 	/**/
 
-	/**/
+/*out of date*/
 	$scope.isOutdated = function(item) {
 		var date = 	new Date(item.enspiredDate);
 		var currentDate = new Date();
 		return ((currentDate - date) > 0);
 	};
+/*sort caption*/
+	$scope.sort = function(fieldName){
+	    $scope.reverse = ($scope.fieldName === fieldName) ? !$scope.reverse : false;
+	    $scope.fieldName = fieldName;
+	};
+	
+/*icon for sort*/
+	$scope.isSortUp = function(fieldName){
+		return $scope.fieldName === fieldName && !$scope.reverse;
+	}
+	$scope.isSortDown = function(fieldName){
+		return $scope.fieldName === fieldName && $scope.reverse;
+	};
+
+
+
+
 
 
 
